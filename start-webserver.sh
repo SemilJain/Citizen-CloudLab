315a65a8fd6133701c9c2e1d5ab98bcd0ecbbdc8
Semil Jain <36932883+SemilJain@users.noreply.github.com>
2024-03-23 16:14:12 -0600

    Create start-webserver.sh

---
+++ start-webserver.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install Nginx web server
+sudo apt update
+sudo apt install nginx -y
+
+# git clone https://github.com/SemilJain/Citizen-CloudLab.git
+# cd Citizen-CloudLab
+
+sudo mkdir -p /var/www/citizen-cloudlab/html
+sudo chown -R $USER /var/www/citizen-cloudlab/html
+sudo chmod -R 755 /var/www/citizen-cloudlab
+
+# Copy HTML/CSS/JavaScript files to the destination directory
+sudo cp -r /local/repository/* /var/www/citizen-cloudlab/html/
+sudo cp /local/repository/citizen-cloudlab-nginx /etc/nginx/sites-available/citizen-cloudlab
+
+sudo ln -s /etc/nginx/sites-available/citizen-cloudlab /etc/nginx/sites-enabled/
+
+# Start Nginx web server
+sudo systemctl restart nginx
+
+# Check if Nginx is running
+if systemctl is-active --quiet nginx ; then
+    echo "Nginx web server is running on port 8083"
+else
+    echo "Failed to start Nginx web server."
+fi
